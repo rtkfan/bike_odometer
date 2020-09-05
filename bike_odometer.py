31ef53529596f0f872acaae254d9645c05716f06
Matt Lam <59208036+rtkfan@users.noreply.github.com>
2020-09-05 14:25:08 -0700

    Move token refresh routine out of main

--- bike_odometer.py
+++ bike_odometer.py
@@ -8,6 +8,7 @@ import logging
 
 endpoint_token = 'https://www.strava.com/oauth/token'
 
+
 def check_envvars():
     clientid = os.getenv('STRAVA_CLIENTID')
     clientsecret = os.getenv('STRAVA_CLIENTSECRET')
@@ -20,25 +21,14 @@ def check_envvars():
     return clientid, clientsecret, athleteid
 
 
-def main():
+def get_access_token(connection, clientid, clientsecret, athleteid):
 
-    logging.basicConfig(format='%(levelname)s %(asctime)s: %(message)s',
-                        level=logging.INFO,
-                        datefmt='%H:%M:%S')
-
-    strava_clientid, strava_clientsecret, athlete_id = check_envvars()
-
-    # connect to db
-    con = sqlite3.connect('./data/odometer.db')
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
+    cur = connection.cursor()
 
     logging.info('Connection established to DB')
-
-    # check/refresh latest access token for athlete
     cur.execute("""SELECT * FROM strava_access_token
                    WHERE athlete_id = ?
-                   ORDER BY expires_at DESC LIMIT 1""", [str(athlete_id)])
+                   ORDER BY expires_at DESC LIMIT 1""", [str(athleteid)])
     last_auth = cur.fetchone()
 
     last_auth_expire_utc = datetime.datetime.utcfromtimestamp(
@@ -48,15 +38,15 @@ def main():
     # humanize assumes the opposite!
     ess_or_dee = 'd' if last_auth_ttl.total_seconds() < 0 else 's'
     logging.info('%s %s %s %s',
-                 'Access token with latest expiry date for athlete',
+                 'Latest access token for athlete',
                  last_auth['athlete_id'], 'expire'+ess_or_dee,
                  humanize.naturaltime(-1*last_auth_ttl))
 
     if last_auth_ttl.total_seconds() <= 3600:  # less than an hour to expiry:
         print('Latest access token has/is close to expiring')
 
-        payload = {'client_id': strava_clientid,
-                   'client_secret': strava_clientsecret,
+        payload = {'client_id': clientid,
+                   'client_secret': clientsecret,
                    'refresh_token': last_auth['refresh_token'],
                    'grant_type': 'refresh_token'}
         r = requests.post(endpoint_token, params=payload)
@@ -70,11 +60,31 @@ def main():
                        VALUES (?,?,?,?)""",
                     [last_auth['athlete_id'], response['access_token'],
                      response['expires_at'], response['refresh_token']])
-        con.commit()
-
+        connection.commit()
+        valid_access_token = response['access_token']
     else:
-        print('auth is OK, use it as is')
+        valid_access_token = last_auth['access_token']
 
+    return valid_access_token
+
+
+def main():
+
+    logging.basicConfig(format='%(levelname)s %(asctime)s: %(message)s',
+                        level=logging.INFO,
+                        datefmt='%H:%M:%S')
+
+    strava_clientid, strava_clientsecret, athlete_id = check_envvars()
+
+    # connect to db
+    con = sqlite3.connect('./data/odometer.db')
+    con.row_factory = sqlite3.Row
+
+    access_token = get_access_token(con, strava_clientid, strava_clientsecret,
+                                    athlete_id)
+    logging.info('%s %s', 'Fetched access token ', access_token)
+
+    # close up shop
     con.close()
 
 
