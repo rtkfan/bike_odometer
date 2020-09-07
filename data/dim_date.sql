166a93d74e6cc0ff95af2ed68f639d343bcf9890
Matt Lam <59208036+rtkfan@users.noreply.github.com>
2020-09-06 19:54:07 -0700

    Add calendar table for easier analytics calcs

---
+++ data/dim_date.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "dim_date" ("the_date" TEXT,
+                         "year" INTEGER,
+                         "month" INTEGER,
+                         "day" INTEGER,
+                         "week" INTEGER,
+                         "day_of_week" INTEGER,
+                         "yearmonth" TEXT
+                       )
