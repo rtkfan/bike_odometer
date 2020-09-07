71498069b282267643238c4c1b440fcdec1feb40
Matt Lam <59208036+rtkfan@users.noreply.github.com>
2020-09-07 16:32:22 -0700

    Add bike_id to installation and apply FK constraints

--- data/installation.sql
+++ data/installation.sql
@@ -1,5 +1,8 @@
 CREATE TABLE "installation" ("id" integer,
+  "bike_id" int,
   "component_id" int,
   "start_date" text,
   "end_date" text,
-  PRIMARY KEY ("id"))
+  PRIMARY KEY ("id"),
+  FOREIGN KEY("bike_id") REFERENCES bike("id"),
+  FOREIGN KEY("component_id") REFERENCES component("id"))
--- data/maintenance_event.sql
+++ data/maintenance_event.sql
@@ -3,4 +3,5 @@ CREATE TABLE "maintenance_event" ("id" integer,
   "event_type" text,
   "bike_id" int,
   "details" text,
-  PRIMARY KEY ("id"))
+  PRIMARY KEY ("id"),
+  FOREIGN KEY("bike_id") REFERENCES bike("id"))
